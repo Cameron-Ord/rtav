30412854fe9d33435beee5afafd64cc5d1397cfc
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2025-06-04 19:24:08 -0600

    first commit

---
+++ shader/frag.fs
@@ -0,0 +1,8 @@
+#version 330 core
+out vec4 frag_colour;
+uniform vec4 colour;
+
+void main()
+{
+    frag_colour = colour;
+}
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("%s\n", SHADER_PATH);
+    return 0;
+}
