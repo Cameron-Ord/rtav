1890e02369364b3fb2b8a06701a8ee1d6c6db3e2
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2025-06-06 20:27:41 -0600

    barebones python build script

---
+++ install.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+import subprocess
+
+os.makedirs("/usr/local/share/3dmv", exist_ok=True)
+
+shutil.copy("shader/vert.vs", "/usr/local/share/3dmv/vert.vs")
+shutil.copy("shader/frag.fs", "/usr/local/share/3dmv/frag.fs")
+
+result = subprocess.run(["cmake -B build && cmake --build build"], shell=True)
+
+if result.returncode == 0:
+    shutil.copy("build/3DMV", "/usr/local/bin/3DMV")
